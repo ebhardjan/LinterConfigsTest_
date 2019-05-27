00ba4889dffc2123b67cf81cdc142b54aebf4bd3
Jan Eberhardt <jan@deepcode.ai>
2019-05-27 14:59:53 +0200

    adding customre_sample.js

---
+++ customer_sample.js
@@ -0,0 +1,15 @@
+function test(a, b) {
+    var  c = a;
+    c++
+
+    let pera = true;
+    if (!pera) {console.log('djoka')}
+
+    var mika = () => {}
+    const djora = 'mika' + 2;
+
+    b = a++;
+    return b + a;
+
+    console.log(pera);
+}
